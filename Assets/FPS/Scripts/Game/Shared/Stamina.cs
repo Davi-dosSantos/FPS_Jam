528fd9a030bfbf672955c43907e8d4e4a9a89355
Marcelo Cavalca Filho <m.cavalca@hotmail.com>
2021-07-10 19:26:30 -0300

    Stamina control script

---
+++ Assets/FPS/Scripts/Game/Shared/Stamina.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Stamina : MonoBehaviour
+{
+    [Tooltip("Maximum amount of stamina")] public float MaxStamina = 100f;
+
+    [Tooltip("Stamina ratio at which the critical stamina vignette starts appearing")]
+    public float CriticalStaminaRatio = 0.3f;
+    
+    private float StaminaLossPerFrame = .4f;
+    private float StaminaGainPerFrame = .2f;
+
+    public float CurrentStamina { get; set; }
+
+    private bool CanGetStamina { get; set; }
+    private float Timer;
+    private float TimerToRegenerate;
+
+    void Start()
+    {
+        CurrentStamina = MaxStamina;
+        TimerToRegenerate = 10f;
+    }
+
+    public void RegenerateStamina() {
+        if (this.CurrentStamina < MaxStamina)
+            this.CurrentStamina += StaminaGainPerFrame;
+    }
+
+    public void LoseStamina() {
+        if (this.CurrentStamina > 0)
+            this.CurrentStamina -= StaminaLossPerFrame;
+    }
+
+    public void StaminaTimer() {
+        Timer += 0.2f;
+        if (Timer > TimerToRegenerate) {
+            CanGetStamina = true;
+        } else { CanGetStamina = false; }
+    }
+
+    public void ResetTimer() {
+        Timer = 0f;
+    }
+
+    public bool CanRegenerate () {
+        return this.CanGetStamina;
+    }
+
+}
